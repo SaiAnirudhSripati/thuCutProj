3faa7c6d941206822ed4e33033afe85644dbd486
Sai Anirudh <saianirudh92@gmail.com>
2020-02-02 19:21:19 +0800

    Added more validation for forms

--- the_cut_project/listApp/forms.py
+++ the_cut_project/listApp/forms.py
@@ -7,3 +7,22 @@ class NewListItemForm(forms.ModelForm):
     class Meta:
         model=List
         fields=['list_name','list_desc']
+
+    def clean(self):
+
+
+        super(NewListItemForm, self).clean()
+
+
+        list_name = self.cleaned_data.get('list_name')
+        list_desc = self.cleaned_data.get('list_desc')
+
+
+        if len(list_name) < 3:
+            self._errors['list_name'] = self.error_class([
+                'Minimum 3 characters required'])
+        if len(list_desc) <10:
+            self._errors['list_desc'] = self.error_class([
+                'Minimum 3 characters required'])
+
+        return self.cleaned_data
--- the_cut_project/listApp/models.py
+++ the_cut_project/listApp/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class Categories(models.Model):
     cat_name=models.CharField(max_length=30,unique=True)
-    cat_description=models.CharField(max_length=120,unique=False)
+    cat_description=models.CharField(max_length=30,unique=False)
     cat_views=models.IntegerField(default=0)
 
 class List(models.Model):
     cat_id=models.IntegerField()
     list_name=models.CharField(max_length=20,unique=False)
-    list_desc=models.CharField(max_length=20,unique=False)
+    list_desc=models.CharField(max_length=120,unique=False)
     list_views=models.IntegerField(default=0)
 
 class PageVisits(models.Model):
